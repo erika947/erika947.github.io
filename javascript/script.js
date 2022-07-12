ce8b180e28cd11bbd2db4be3d63fdaafe397745a
Erika Rivera <rivera.erika07@gmail.com>
2022-07-12 14:10:50 -0400

    1st commit

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+//create a game of tic tac toe
+//theme will be under the sea
+//use different shapes instead of x's and o's. decided to go with starfishes and sand dollars
+//board color a dark blue
+//under water background
+
+//player1 and player2 or computer(need to decided which one)
+//player 1 picks first
+//player 1 is the starfish
+//the round will keep going until either one wins or it is a tie
