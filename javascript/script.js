3a5bb6e7b07f039ed4fe48f3c2d42bc13cafddf2
Erika Rivera <rivera.erika07@gmail.com>
2022-07-20 01:00:11 -0400

    New Update

--- javascript/script.js
+++ javascript/script.js
@@ -8,16 +8,77 @@
 //player 1 picks first
 //player 1 is the starfish
 //the round will keep going until either one wins or it is a tie
+
+
 const gameBoxes = document.querySelectorAll('.box')
+const statusDiv = document.querySelector('.status')
+const resetDiv = document.querySelector('.reset')
 
+const player1 = 'StarFish'
+const player2 ='SandDollar'
 
-// const player1 = 'starfish'
-// const player2 = 'sand dollar'
+let gameLive = true
 let starfishStarts = true
 
+//Button to restart the whole game
+const fullReset = (e) => {
+    starfishStarts =true
+    statusDiv.innerHTML = `${player1} turn`
+    for(const gameBox of gameBoxes){
+        gameBox.classList.remove('starfish')
+        gameBox.classList.remove('sandDollar')
+    }
+}
+//Create a function that updates the Status bar
+
+const playerStatus = (team) => {}//fill out
+
+const whoWon = (team) => {
+    gameLive = false
+    if (team === 'StarFish'){
+        statusDiv.innerText = `${playerStatus(team)} has won`
+    }else {
+        statusDiv.innerText = `${playerStatus(team)} has won!`;
+        }
+    }
+
+
+// this will check the postions of where the player can win
+const statusCheck = () => {
+    const topL = gameBoxes[0].classList[2]
+    const topM = gameBoxes[1].classList[2]
+    const topR = gameBoxes[2].classList[2]
+    const midL = gameBoxes[3].classList[2]
+    const midM = gameBoxes[4].classList[2]
+    const midR = gameBoxes[5].classList[2]
+    const bottomL = gameBoxes[6].classList[2]
+    const bottomM = gameBoxes[7].classList[2]
+    const bottomR = gameBoxes[8].classList[2]
+    // check for the winner
+    if(topL && topL === topM && topL === topR){//top row
+    whoWon(topL)
+
+    }else if(midL && midL === midM && midL === midR){//Middle row
+        whoWon(midL)
+
+    } else if( bottomL && bottomL === bottomM && bottomL === bottomR){//Bottom row
+        whoWon(bottomL)
+
+    }else if(topL && topL === midL && topL === bottomL){//left column
+        whoWon(topL)
+
+    }else if(topM && topM === midM && topM === bottomM ){//Middle Column
+        whoWon(topM)
+
+    }else if(topR && topR === midR && topR === bottomR){//Right Column
+        whoWon(topR)
+
+    }//add rest of the combinations as well the outcome for a draw
+}
+//create a function to click in the squares
 const boxclick = (e) =>{
     const listTurn = e.target.classList
-    const location = listTurn[1]
+
 
     if (listTurn[2] === 'starfish' || listTurn[2] === 'sandDollar'){//this allows only one player to place in this square
         return
@@ -25,22 +86,21 @@ const boxclick = (e) =>{
 
     if (starfishStarts){
         listTurn.add('starfish'),
-        starfishStarts = !starfishStarts //which means that is the other players turn
+         //which means that is the other players turn
+        statusCheck()
     }
     else{
         listTurn.add('sandDollar'),
-        starfishStarts = !starfishStarts
+        
+        statusCheck()
     }
 }
-const winningArray = [
-    [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]// these are the postions of where the player can win
- 
-
-//need to check who one the game
 
 for(const gameBoxe of gameBoxes){
     gameBoxe.addEventListener('click', boxclick)
 }
+
+resetDiv.addEventListener('click',fullReset)
 
 
 
